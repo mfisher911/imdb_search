3011eca10ed3b89855459f328c6b8833b9d99f28
Mike Fisher <mfisher911@gmail.com>
2020-10-31 14:52:05 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # imdb_search
-Scrape IMDb for Day One entry templates
+
+Scrape IMDb for Day One entry templates.
+
+Expects a POSTed form with a variable "input", which can either be a
+URL (from "Copy URL") or the text from an iOS Share Sheet action.
+
+Returns a JSON dictionary with the following keys:
+
+- title -- IMDb page title
+- url -- IMDb page URL
+- summary -- Full IMDb summary
+- letterbox_title -- title with the year removed
+
+## Deployment Notes
+
+Runs behind a reverse proxy with the following tooling:
+
+- uWSGI application server
+- Supervisor Process Control System
---
+++ application.py
@@ -0,0 +1,84 @@
+import logging
+from logging.config import dictConfig
+
+import requests
+
+from bs4 import BeautifulSoup
+from flask import Flask, request, jsonify
+
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": ("[%(asctime)s] %(levelname)s in "
+                           "%(module)s: %(message)s"),
+            }
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://flask.logging.wsgi_errors_stream",
+                "formatter": "default",
+            }
+        },
+        "root": {"level": "INFO", "handlers": ["wsgi"]},
+    }
+)
+
+app = Flask(__name__)
+
+
+@app.route("/imdb/", methods=["GET", "POST"])
+def hello_world():
+    if request.form:
+        input = request.form.get("input")
+        logging.info("got input: %s", input)
+        title = ""
+        _sum = ""
+        url = ""
+        parts = input.split("\r\n")
+        if len(parts) == 3:
+            title, _sum, url = parts
+        elif len(parts) == 2:
+            title, url = parts
+        elif len(parts) == 1:
+            url = input
+        if len(url) == 0:
+            return "No URL was provided", 400
+        url = url.replace("\\/", "/")
+
+        imdb = requests.get(url)
+        soup = BeautifulSoup(imdb.content, "html5lib")
+        title = soup.title.text.replace(" - IMDb", "")
+        summary = ""
+        try:
+            summary = soup.find_all("div", class_="summary_text")[
+                0
+            ].text.strip()
+        except IndexError:
+            summary = _sum
+
+        # strip the year from the end for letterboxd.com
+        yindex = title.rfind("(")
+        if yindex != -1:
+            lb_title = title[0 : yindex - 1]
+        else:
+            lb_title = title
+
+        result = {
+            "title": title,
+            "url": url,
+            "summary": summary,
+            "letterboxd_title": lb_title,
+        }
+        logging.info("returning result: %s", result)
+
+        return jsonify(result)
+    else:
+        return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
