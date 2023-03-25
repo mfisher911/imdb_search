bde70505a718bafb924763563d12d2f6d6db9c9f
Mike Fisher <mfisher911@gmail.com>
2023-03-25 15:49:55 -0400

    add Google Sheets use argument

--- apptest.py
+++ apptest.py
@@ -13,11 +13,14 @@ logger = logging.getLogger(__name__)
 def main():
     result = application.process(args.url)
     pprint.pprint(result)
+    if args.sheets:
+        application.log_to_sheets(result["title"])
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("url")
+    parser.add_argument("--sheets", action="store_true", default=False)
     args = parser.parse_args()
 
     main()
