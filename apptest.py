af38bc824562cb82cee738651b67cf2679545eff
Mike Fisher <mfisher911@gmail.com>
2025-02-21 20:23:15 -0500

    feat: add testing argument for Trakt

--- apptest.py
+++ apptest.py
@@ -11,16 +11,21 @@ logger = logging.getLogger(__name__)
 
 
 def main():
+    application.debug = True  # disable sending debug emails
     result = application.process(args.url)
     pprint.pprint(result)
     if args.sheets:
         application.log_to_sheets(result["title"])
+
+    if args.trakt:
+        application.trakt_log(args.url)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("url")
     parser.add_argument("--sheets", action="store_true", default=False)
+    parser.add_argument("--trakt", action="store_true", default=False)
     args = parser.parse_args()
 
     main()
